7fbd8ca1c202649fb1f5b91a792c4caa658cc5d1
Hassan Shafqat <muhammadhshafqat@gmail.com>
2026-07-13 09:10:22 +0500

    react setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Dock from "./components/dock";
+function App() {
+  return (
+    <>
+      <Dock />
+    </>
+  );
+}
+
+export default App;
